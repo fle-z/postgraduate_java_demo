2a2940404582b9440420c2275729932d7fd8e1ee
fle <1509182803@qq.com>
2019-04-30 11:26:54 +0800

    并发编程的艺术：线程池以及AQS的demo

---
+++ src/ThreadPool/DefaultThreadPool.java
@@ -0,0 +1,128 @@
+package ThreadPool;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicLong;
+
+public class DefaultThreadPool<Job extends Runnable> implements ThreadPool<Job>{
+    // 线程池最大限制数
+    private static final int MAX_WORKER_NUMBERS = 10;
+    // 线程池默认的数量
+    private static final int DEFAULT_WORKER_NUMBERS = 5;
+    // 线程池最小的数量
+    private static final int MIN_WORKER_NUMBERS = 1;
+
+    // 这是一个工作链表，将会向里面插入工作
+    private final LinkedList<Job> jobs = new LinkedList<Job>();
+    // 工作者列表
+    private final List<Worker> workers = Collections.synchronizedList(new ArrayList<Worker>());
+    // 工作者线程的数量
+    private int workerNum = DEFAULT_WORKER_NUMBERS;
+    // 线程编号的生成
+    private AtomicLong threadNum = new AtomicLong();
+
+    public DefaultThreadPool() {
+        this(DEFAULT_WORKER_NUMBERS);
+    }
+
+    public DefaultThreadPool(int num) {
+        workerNum = num > MAX_WORKER_NUMBERS? MAX_WORKER_NUMBERS :
+                num < MIN_WORKER_NUMBERS? MIN_WORKER_NUMBERS : num;
+
+        initializeWorkers(num);
+    }
+
+    public void execute(Job job) {
+        if (job != null) {
+            //添加一个工作然后进行通知
+            synchronized (jobs) {
+                jobs.addLast(job);
+                jobs.notify();
+            }
+        }
+    }
+
+    public void shutdown() {
+        for (Worker worker : workers) {
+            worker.shutdown();
+        }
+    }
+
+    public void addWorkers(int num){
+        synchronized (jobs) {
+            if (num + this.workerNum > MAX_WORKER_NUMBERS) {
+                num = MAX_WORKER_NUMBERS - this.workerNum;
+            }
+            initializeWorkers(num);
+            this.workerNum += num;
+        }
+    }
+
+    public void removeWorker(int num) {
+        synchronized (jobs) {
+            if (num >= this.workerNum) {
+                throw new IllegalArgumentException("beyond workNum");
+            }
+            // 按照给定的数量停止Worker
+            int count = 0;
+            // 注意！这里用的是while循环而不是for循环，原因应该是考虑remove失败
+            while (count < num) {
+                Worker worker = workers.get(count);
+                if (workers.remove(worker)) {
+                    worker.shutdown();
+                    count++;
+                }
+            }
+            this.workerNum -= count;
+        }
+    }
+
+    public int getJobSize() {
+        return jobs.size();
+    }
+
+    private void initializeWorkers(int num) {
+        for (int i = 0; i < num; i++) {
+            Worker worker = new Worker();
+            workers.add(worker);
+            Thread thread = new Thread(worker, "ThreadPool-Worker-" + threadNum.incrementAndGet());
+            thread.start();
+        }
+    }
+
+    class Worker implements Runnable {
+        private volatile boolean running = true;
+        public void run() {
+            while (running) {
+                Job job = null;
+                synchronized (jobs) {
+                    while (jobs.isEmpty()) {
+                        try {
+                            jobs.wait();
+                        } catch (InterruptedException e) {
+                            // 感知到外部对WorkerThread的中断操作，返回
+                            Thread.currentThread().interrupt();
+                            return;
+                        }
+                    }
+                    job = jobs.removeFirst();
+                }
+                if (job != null) {
+                    try {
+                        job.run();
+                    } catch (Exception e) {
+                        // 忽略Job执行中的Exception
+                    }
+                }
+            }
+        }
+
+        public void shutdown() {
+            running = false;
+        }
+    }
+}
+
+
