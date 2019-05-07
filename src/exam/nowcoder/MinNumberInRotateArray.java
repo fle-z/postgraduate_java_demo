94e2270c057e941701df4ac9df246e183fbf4a12
fle <1509182803@qq.com>
2019-05-07 19:34:38 +0800

    剑指offer：旋转数组的最小数字

--- src/exam/nowcoder/MinNumberInRotateArray.java
+++ src/exam/nowcoder/MinNumberInRotateArray.java
@@ -1,7 +1,39 @@
 package exam.nowcoder;
 
+import java.util.Arrays;
+
 public class MinNumberInRotateArray {
     public int minNumberInRotateArray(int [] array) {
-        return  0;
+        int len = array.length;
+        if (len == 0)
+            return  0;
+        int head = 0, tail = len - 1;
+        int res = Integer.MAX_VALUE;
+        while (head < tail){
+            if (array[head] != array[tail]){
+                int mid = (head + tail) / 2;
+                if(array[mid] > array[tail]){
+                    head = mid + 1;
+                } else {
+                    tail = mid;
+                }
+                res = array[tail];
+            } else {
+                for (int i = head; i < tail; i++){
+                    if(array[i] < res)
+                        res = array[i];
+                }
+
+                break;
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] array = new int[]{3, 4, 5, 1, 2};
+        MinNumberInRotateArray m = new MinNumberInRotateArray();
+        System.out.println(m.minNumberInRotateArray(array));
     }
 }
